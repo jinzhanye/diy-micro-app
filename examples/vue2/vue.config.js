627f6705c336b03bc5e546e54f3399913c9a7a94
yejinzhan <jinzhan.ye@foxmail.com>
2022-12-19 13:04:10 +0800

    添加 react、vue 应用

---
+++ examples/vue2/vue.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set("diy-micro-app", path.join(__dirname, '../../src/index.js'))
+  },
+})
