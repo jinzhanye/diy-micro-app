197e021952846c9920f90f5e898809f0a898dab3
yejinzhan <jinzhan.ye@foxmail.com>
2022-12-19 17:33:41 +0800

    update

--- examples/vue2/vue.config.js
+++ examples/vue2/vue.config.js
@@ -3,6 +3,12 @@ const path = require('path')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080, // 端口号
+    https: false, // https:{type:Boolean}
+    proxy: 'http://localhost:3000' // 没有将子应用的静态资源替换成绝对路径，这里临时解决方案，利用 proxy 将所有找不到的静态资源请求到子应用，也就是 localhost:3000
+  },
   chainWebpack: config => {
     config.resolve.alias
       .set('diy-micro-app', path.join(__dirname, '../../src/index.js'))
--- src/element.js
+++ src/element.js
@@ -1,3 +1,5 @@
+import CreateApp from './app'
+
 class MyElement extends HTMLElement {
   // 声明需要监听的属性名，只有这些属性变化时才会触发attributeChangedCallback
   static get observedAttributes () {
@@ -11,6 +13,13 @@ class MyElement extends HTMLElement {
   connectedCallback() {
     // 元素被插入到DOM时执行，此时去加载子应用的静态资源并渲染
     console.log('micro-app is connected')
+
+    // 创建微应用实例
+    const app = new CreateApp({
+      name: this.name,
+      url: this.url,
+      container: this,
+    })
   }
 
   disconnectedCallback () {
@@ -18,9 +27,16 @@ class MyElement extends HTMLElement {
     console.log('micro-app has disconnected')
   }
 
-  attributeChangedCallback (attr, oldVal, newVal) {
+  attributeChangedCallback (attrName, oldVal, newVal) {
     // 元素属性发生变化时执行，可以获取name、url等属性的值
-    console.log(`attribute ${attr}: ${newVal}`)
+    console.log(`attribute ${attrName}: ${newVal}`)
+
+    // 分别记录name及url的值
+    if (attrName === 'name' && !this.name && newVal) {
+      this.name = newVal
+    } else if (attrName === 'url' && !this.url && newVal) {
+      this.url = newVal
+    }
   }
 }
 
