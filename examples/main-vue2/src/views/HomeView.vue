ac06a6c9819372fde962bdb936869f1c579b44ba
yejinzhan <jinzhan.ye@zhenai.com>
2023-02-09 10:35:46 +0800

    修改目录名称

---
+++ examples/main-vue2/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <micro-app name='app' url='http://localhost:3000/'></micro-app>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {}
+}
+</script>
